375a86e92e50b35a651d8ec72725f6d376665a50
SameerAhmed1402 <142417658+SameerAhmed1402@users.noreply.github.com>
2023-08-28 14:56:25 +0530

    Add files via upload

---
+++ Json.js
@@ -0,0 +1,5 @@
+let resume={
+    {"name":"sameer",
+    "address":"33 A/3 Nabigal Nayagam Street,Pattapathu,Tirunelveli Town",
+    "Skills":{"C":"intermediate","Java":"Advance","Javascript":"Beginner"}}
+}
